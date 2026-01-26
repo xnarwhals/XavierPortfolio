1227d5cacdd8dcf4bafb4fa0fe728062e62b9f76
Xavier Gonzalez <102434421+xnarwhals@users.noreply.github.com>
2026-01-25 16:02:23 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  base: '/XavierPortfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
